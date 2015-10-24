e356c38a974e8b8de44ada35a77f93b5f95efde6
Greg <gregory.wood@sheridancollege.ca>
2015-10-24 16:42:57 -0400

    Adds ex1_05 and ex1_08

--- ch1/ex1_05.c
+++ ch1/ex1_05.c
@@ -1,6 +1,59 @@
 /*
+ * Data Structures in C by Noel Kalicharan
+ * Chapter 1, exercise 5
+ *
  * Write a function which, given two date structures, d1 and d2, returns
  * -1 if d1 comes before d2, 0 if d1 is the same as d2 and 1 if d1 comes
  * after d2.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct date {
+    char day, month;
+    int year;
+} Date;
+
+int compareDate(Date*, Date*);
+Date* initDate(char, char, int);
+
+int main() {
+    int y;
+    char d, m;
+    Date *d1, *d2;
+    printf("Enter date one (DD MM YYYY):");
+    scanf("%d %d %d", &d, &m, &y);
+    d1 = initDate(d, m, y);
+
+    printf("Enter date two (DD MM YYYY):");
+    scanf("%d %d %d", &d, &m, &y);
+    d2 = initDate(d, m, y);
+
+    printf("Compare dates: %d\n", compareDate(d1, d2));
+    return;
+}
+
+int compareDate(Date *d1, Date *d2) {
+    if (d1->year < d2->year) 
+        return -1;
+    else if (d1->year > d2->year)
+        return 1;
+    else if (d1->month < d2->month)
+        return -1;
+    else if (d1->month > d2->month)
+        return 1;
+    else if (d1->day < d2->day)
+        return -1;
+    else if (d1->day > d2->day)
+        return 1;
+    else 
+        return 0;
+}
+
+Date* initDate(char day, char month, int year) {
+    Date* d = (Date*) malloc(sizeof(Date));
+    d->day = day;
+    d->month = month;
+    d->year = year;
+}
--- ch1/ex1_08.c
+++ ch1/ex1_08.c
@@ -1,6 +1,79 @@
 /*
+ * Data Structures in C by Noel Kalicharan
+ * Chapter 1 Exercise 8
+ *
  * A length, specified in metres and centimeters, is represented by two
  * integers. For example, the length 3m 75cm is represented by 3 75. Using
  * a structure to represent a length, write functions to compare, add and
  * subtract two lengths.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct length {
+    int meter;
+    int centimeter;
+} Length;
+
+Length* makeLength(int, int);
+int compareLength(Length*, Length*);
+Length* addLength(Length*, Length*);
+Length* subtractLength(Length*, Length*);
+
+int main() {
+    int x, y;
+    Length *l1, *l2, *sum;
+
+    printf("Enter the first length (xx yy): ");
+    scanf("%d %d", &x, &y);
+    l1 = makeLength(x, y);
+
+    printf("Enter the second length (xx yy): ");
+    scanf("%d %d", &x, &y);
+    l2 = makeLength(x, y);
+
+    printf("Compare lengths: %d\n", compareLength(l1, l2));
+    
+    sum = addLength(l1, l2);
+    printf("Length 1 + Length 2 = %d %d\n", sum->meter, sum->centimeter);
+
+    sum = subtractLength(l1, l2);
+    printf("Length 1 - Length 2 = %d %d\n", sum->meter, sum->centimeter);
+}
+
+Length* makeLength(int m, int cm) {
+    Length* l = (Length*)malloc(sizeof(Length));
+    l->meter=m;
+    l->centimeter=cm;
+    return l;
+}
+
+/*
+ * Return 1 if the second number is bigger, -1 for first number is bigger,
+ * 0 for the same.
+ */
+int compareLength(Length *l1, Length *l2) {
+    if (l1->meter > l2->meter)
+        return -1;
+    else  if (l1->meter < l2->meter)
+        return 1;
+    else if (l1->centimeter > l2->centimeter)
+        return -1;
+    else if (l1->centimeter < l2->centimeter)
+        return 1;
+    else
+        return 0;
+}
+
+Length* addLength(Length *l1, Length *l2) { 
+    int m = (l1->meter * 100 + l1->centimeter) + (l2->meter * 100 + l2->centimeter);
+    Length* sum = makeLength(m/100, m%100);
+    return sum;
+}
+
+Length* subtractLength(Length *l1, Length *l2) {
+    int m = (l1->meter * 100 + l1->centimeter) - (l2->meter * 100 + l2->centimeter);
+    Length* sum = makeLength(m/100, m%100);
+    return sum;
+}
